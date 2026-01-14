16cadc4230d5bb944ed813cca83229ff56211058
Joy Kim <thisisjoey77@gmail.com>
2026-01-15 01:57:17 +0900

    Add BOJ Gold solution: 1234.cpp

---
+++ BOJ/Gold/1234.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pii pair<ll, ll>
+#define priqueue priority_queue<pii>
+using namespace std;
+
+ll N;
+ll i, j, k;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> N;
+    cout << "hello world" << N << endl;
+    return 0;
+}
